88efe94ad4dc244f578ac3d395eb2a154d75ddc0
Mario Tomić <mario.tomic@gdi.net>
2019-09-12 16:29:41 +0200

    End of section filter pagging and sorting

--- DatingApp.API/Helpers/PagedList.cs
+++ DatingApp.API/Helpers/PagedList.cs
@@ -8,6 +8,10 @@ namespace DatingApp.API.Helpers
 {
     public class PagedList<T> : List<T>
     {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
         public PagedList(int currentPage, int totalPages, int pageSize, int totalCount) 
         {
             this.CurrentPage = currentPage;
@@ -16,14 +20,11 @@ namespace DatingApp.API.Helpers
             this.TotalCount = totalCount;
                
         }
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
-        public int PageSize { get; set; }
-        public int TotalCount { get; set; }
+
         public PagedList(List<T> items, int count, int pageNumber, int pageSize)
         {
             TotalCount = count;
-            PageSize = PageSize;
+            PageSize = pageSize;
             CurrentPage = pageNumber;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             this.AddRange(items);
